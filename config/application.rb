9b0b58c383e2d8f1704c4d60f0d9fa90ecefb7d5
Ivan Karyakin <ivan.karyaking@gmail.com>
2018-08-31 15:25:35 +0300

    frontend changes

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module App
     config.load_defaults 5.2
     config.exceptions_app = self.routes
 
+    config.i18n.default_locale = :ru
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
